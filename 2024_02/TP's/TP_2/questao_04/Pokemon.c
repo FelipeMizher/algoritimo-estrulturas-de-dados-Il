20f63143ab37e5348fb7e83414cb7f0819951db4
Felipe Mizher <130677681+FelipeMizher@users.noreply.github.com>
2024-10-25 17:46:06 -0300

    Add files via upload

---
+++ 2024_02/TP's/TP_2/questao_04/Pokemon.c
@@ -0,0 +1,351 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include <time.h>
+
+int count = 0;
+float tempo = 0.0;
+clock_t start;
+clock_t end;
+
+typedef struct{
+    int dia;
+    int mes;
+    int ano;
+} Data;
+
+typedef struct{
+    int id;
+    int generation;
+    char name[100];
+    char description[300];
+    char types[2][100];
+    char abilities[6][100];
+    double weight;
+    double height;
+    int captureRate;
+    int isLegendary;
+    Data date;
+} Pokemon;
+
+Pokemon pokemons[801];
+Pokemon sub[801];
+int tamSubArray = 0;
+
+void formatarTipos(char tipos[2][100], char* buffer, int n){
+    strcpy(buffer, "[");
+    int primeiro = 1; 
+    for(int i = 0; i < n; i++){
+        if(strcmp(tipos[i], "") != 0){ 
+            if(!primeiro){
+                strcat(buffer, ", ");
+            }
+            strcat(buffer, "'");
+            strcat(buffer, tipos[i]);
+            strcat(buffer, "'");
+            primeiro = 0;
+        }
+    }
+    strcat(buffer, "]");
+}
+
+void formatarHabilidades(char habilidades[6][100], char* buffer, int n){
+    strcpy(buffer, "[");
+    int primeiro = 1; 
+    for(int i = 0; i < n; i++){
+        if(strcmp(habilidades[i], "") != 0){  
+            if(!primeiro){
+                strcat(buffer, ", ");
+            }
+            strcat(buffer, "'");
+            strcat(buffer, habilidades[i]);
+            strcat(buffer, "'");
+            primeiro = 0;
+        }
+    }
+    strcat(buffer, "]");
+}
+
+void ajustarString(char *str){
+    int start = 0;
+    int end = strlen(str) - 1;
+
+    while(str[start] == ' ' || str[start] == '\t' || str[start] == '\n'){
+        start++;
+    }
+
+    while((end >= start) && (str[end] == ' ' || str[end] == '\t' || str[end] == '\n')){
+        end--;
+    }
+
+    int i;
+    for(i = start; i <= end; i++){
+        str[i - start] = str[i];
+    }
+    str[i - start] = '\0';
+}
+
+char **dividirString(const char *src, const char *delim, int *count){
+    char *buf = NULL;
+    int srclen = strlen(src);
+    int delimlen = strlen(delim);
+    char **array = NULL;
+    *count = 0;
+
+    buf = (char *)malloc(srclen + 1);
+    if(!buf) return NULL;
+
+    strcpy(buf, src);
+
+    char *start = buf;
+    char *end = strstr(start, delim);
+
+    while(end != NULL){
+        array = (char **)realloc(array, (*count + 1) * sizeof(char *));
+        *end = '\0';
+        array[*count] = strdup(start);
+        (*count)++;
+
+        start = end + delimlen;
+        while(strncmp(start, delim, delimlen) == 0){
+            array = (char **)realloc(array, (*count + 1) * sizeof(char *));
+            array[*count] = strdup("");
+            (*count)++;
+            start += delimlen;
+        }
+
+        end = strstr(start, delim);
+    }
+
+    array = (char **)realloc(array, (*count + 1) * sizeof(char *));
+    array[*count] = strdup(start);
+    (*count)++;
+
+    array = (char **)realloc(array, (*count + 1) * sizeof(char *));
+    array[*count] = NULL;
+
+    free(buf);
+    return array;
+}
+
+void liberarDivisao(char **strlist){
+    int i = 0;
+    while(strlist[i]){
+        free(strlist[i++]);
+    }
+    free(strlist);
+}
+
+void lerLinha(char *linha, Pokemon *p){
+    int c = 0;
+    int count = 0;
+    char **array = dividirString(linha, ",", &count); 
+    
+    p->id = atoi(array[c++]);
+    p->generation = atoi(array[c++]);
+    strcpy(p->name, array[c++]);
+    strcpy(p->description, array[c++]);
+
+    strcpy(p->types[0], array[c++]);
+    if(strcmp(array[c], "") != 0){
+        strcpy(p->types[1], array[c++]);
+    } else{
+        strcpy(p->types[1], "");
+        c++;
+    }
+
+    int idxHabilidades = 0;
+    while(idxHabilidades <= 6 && c < count){
+        strcpy(p->abilities[idxHabilidades], array[c]);
+        idxHabilidades++;
+        c++;
+        if(strchr(array[c - 1], ']')){
+            break;
+        }
+    }
+
+    for(int i = 0; i < idxHabilidades; i++){
+        char *habilidade = p->abilities[i];
+        int len = strlen(habilidade);
+        char habilidadeLimpa[100] = "";
+        int idx = 0;
+
+        for(int j = 0; j < len; j++){
+            if(habilidade[j] != '[' && habilidade[j] != ']' && habilidade[j] != '"' && habilidade[j] != '\''){
+                habilidadeLimpa[idx++] = habilidade[j];
+            }
+        }
+        habilidadeLimpa[idx] = '\0';
+        ajustarString(habilidadeLimpa);
+        strcpy(p->abilities[i], habilidadeLimpa);
+    }
+
+    if(strcmp(array[c], "") == 0){
+        p->weight = 0.0;
+    } else{
+        p->weight = atof(array[c]);
+    }
+    c++;
+
+    if(strcmp(array[c], "") == 0){
+        p->height = 0.0;
+    } else{
+        p->height = atof(array[c]);
+    }
+    c++;
+
+    p->captureRate = atoi(array[c++]);
+    p->isLegendary = (strcmp(array[c++], "1") == 0);
+
+    char *data = array[c++];
+    sscanf(data, "%d/%d/%d", &p->date.dia, &p->date.mes, &p->date.ano);
+
+    liberarDivisao(array);
+}
+
+void lerDados(){
+    char path[100];
+    bool header = true;
+    char line[500];
+    int i = 0;
+
+    strcpy(path, "/tmp/pokemon.csv");
+    FILE *file = fopen(path, "r");
+    if(file == NULL){
+        perror("Erro ao abrir o arquivo");
+        return;
+    }
+
+    while(fgets(line, sizeof(line), file)){
+        if(header){
+            header = false;
+        } else{
+            lerLinha(line, &pokemons[i]);
+            i++;
+            if(i >= 801) break; 
+        }
+    }
+    fclose(file);
+}
+
+void swap(Pokemon *a, Pokemon *b){
+    Pokemon temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void clone(Pokemon *p, Pokemon *clone){
+    clone->id = p->id;
+    clone->generation = p->generation;
+    strcpy(clone->name, p->name);
+    strcpy(clone->description, p->description);
+    
+    for(int i = 0; i < 2; i++){
+        strcpy(clone->types[i], p->types[i]);
+    }
+    
+    for(int i = 0; i < 6; i++){
+        strcpy(clone->abilities[i], p->abilities[i]);
+    }
+    
+    clone->weight = p->weight;
+    clone->height = p->height;
+    clone->captureRate = p->captureRate;
+    clone->isLegendary = p->isLegendary;
+    clone->date = p->date; 
+}
+
+void insercao(Pokemon *per, int tam){
+    for(int i = 0; i < (tam - 1); i++){
+        int menor = i;
+        for(int j = (i + 1); j < tam; j++){
+            if(strcmp(per[menor].name, per[j].name) > 0){
+                menor = j;
+            }
+        }
+        swap(&per[menor], &per[i]);
+    }
+}
+
+bool isFim(char str[]){
+    return str[0] == 'F' && str[1] == 'I' && str[2] == 'M';
+}
+
+void subArray(){
+    char id[50];
+
+    scanf(" %s", id);
+    while(strcmp(id, "FIM") != 0){
+        int i = 0;
+        while(i < 801){
+            if(pokemons[i].id == atoi(id)){
+                if(tamSubArray < 801){
+                    clone(&pokemons[i], &sub[tamSubArray]);
+                    tamSubArray++;
+                }
+                break; 
+            }
+            i++;
+        }
+        scanf(" %s", id);
+    }
+    insercao(sub, tamSubArray);
+}
+
+void BuscarBin(){
+    char id[100];
+
+    scanf(" %[^\n\r]", id);
+    start = clock();
+
+    while(!isFim(id)){
+        bool find = false;
+        int left = 0, right = tamSubArray - 1;
+
+        while(left <= right && !find){
+            int mid = (left + right) / 2;
+            int cmp = strcmp(sub[mid].name, id);
+
+            if(cmp == 0){
+                find = true;
+                count++;
+            } else if(cmp < 0){
+                left = mid + 1;
+                count++;
+            } else{
+                right = mid - 1;
+                count++;
+            }
+        }
+
+        if(find){
+            printf("SIM\n");
+        } else{
+            printf("NAO\n");
+        }
+        scanf(" %[^\n\r]", id);
+    }
+    end = clock();
+    tempo = ((double)(end - start)) / CLOCKS_PER_SEC;
+}
+
+void registroLog(){
+    FILE *arquivo;
+
+    arquivo = fopen("/tmp/821811_binaria.txt", "w");
+
+    fprintf(arquivo, "Matrícula: 821811\t Tempo de execução: %.6f segundos\t Número de comparações: %d", tempo, count);
+
+    fclose(arquivo);
+}
+
+int main(){
+
+    lerDados(); 
+    subArray();
+    BuscarBin();
+    registroLog();
+
+    return 0;
+}
